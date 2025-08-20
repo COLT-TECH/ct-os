885ff8784b042e08793599eef2f76101211c41d1
COLT-TECH <cdodson218@gmail.com>
2025-08-19 21:50:50 -0400

    .

--- text-mode/kernel/kernel.c
+++ text-mode/kernel/kernel.c
@@ -3,10 +3,13 @@
 #include <stdint.h>
 
 #include "../klib/include/standard.h"
+#include "../klib/include/terminal.h"
+#include "./snake.c"
 
 void kernel_main(void) {
-    
+    terminal_init();
+
     while (1) {
-        
+        snake();
     }
 }
